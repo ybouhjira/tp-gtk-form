1c947a02ed91129ad39ca91e3510c901e52a4545
ybouhjira <b.youssef91@gmail.com>
2014-04-06 04:52:13 +0100

    ecrire_etudiant

--- file.h
+++ file.h
@@ -10,25 +10,50 @@ Etudiant* lire_etudiant(FILE *fichier)
 
     while(fgetc(fichier) != '\n');
 
-    fscanf(fichier, "\tcne : %s", etudiant->cne);
-    fscanf(fichier, "\tnom : %s", etudiant->nom);
-    fscanf(fichier, "\tprenom : %s", etudiant->prenom);
-    fscanf(fichier, "\tcne : %s", etudiant->cne);
-    fscanf(fichier, "\tmot de passe : %s", etudiant->motDePasse);
-    fscanf(fichier, "\tcin : %s", etudiant->cin);
-    fscanf(fichier, "\tnotes : %s", etudiant->cne);
-    fscanf(fichier, "\tdiplome : %s", etudiant->diplome);
+    fscanf(fichier, "\tcne : %s", &etudiant->cne);
+    fscanf(fichier, "\tnom : %s", &etudiant->nom);
+    fscanf(fichier, "\tprenom : %s", &etudiant->prenom);
+    fscanf(fichier, "\tcne : %s", &etudiant->cne);
+    fscanf(fichier, "\tmot de passe : %s", &etudiant->motDePasse);
+    fscanf(fichier, "\tcin : %s", &etudiant->cin);
+    fscanf(fichier, "\tnotes : %s", &etudiant->cne);
+    fscanf(fichier, "\tdiplome : %s", &etudiant->diplome);
 
     fscanf(fichier, "\tnotes : ");
-    int nbrNotes = nombre_notes_diplome(etudiant->diplome), i;
+    int nbrNotes = nombre_notes_diplome(&etudiant->diplome), i;
     for(i = 0; i < nbrNotes; i++)
-        fscanf(fichier, "%f,", etudiant->notes[i]);
+        fscanf(fichier, "%f,", &etudiant->notes[i]);
 
-    fscanf(fichier, "\tetablissement : %s", etudiant->etab);
-    fscanf(fichier, "\tnombre d'années : %d", etudiant->nbrAns);
-    fscanf(fichier, "\tannée d'obtention' : %s", etudiant->anDiplome);
+    fscanf(fichier, "\tetablissement : %s", &etudiant->etab);
+    fscanf(fichier, "\tnombre d'années : %d", &etudiant->nbrAns);
+    fscanf(fichier, "\tannée d'obtention' : %s", &etudiant->anDiplome);
 
     return etudiant;
+}
+
+void ecrire_etudiant(FILE *fichier, int num, Etudiant *etudiant)
+{
+
+    fprintf("%d : ", num);
+
+    fprintf(fichier, "\tcne : %s", etudiant->cne);
+    fprintf(fichier, "\tnom : %s", etudiant->nom);
+    fprintf(fichier, "\tprenom : %s", etudiant->prenom);
+    fprintf(fichier, "\tcne : %s", etudiant->cne);
+    fprintf(fichier, "\tmot de passe : %s", etudiant->motDePasse);
+    fprintf(fichier, "\tcin : %s", etudiant->cin);
+    fprintf(fichier, "\tnotes : %s", etudiant->cne);
+    fprintf(fichier, "\tdiplome : %s", etudiant->diplome);
+
+    fprintf(fichier, "\tnotes : ");
+    int nbrNotes = nombre_notes_diplome(etudiant->diplome), i;
+    for(i = 0; i < nbrNotes; i++)
+        fprintf(fichier, "%f,", etudiant->notes[i]);
+
+    fprintf(fichier, "\tetablissement : %s", etudiant->etab);
+    fprintf(fichier, "\tnombre d'années : %d", etudiant->nbrAns);
+    fprintf(fichier, "\tannée d'obtention' : %s", etudiant->anDiplome);
+
 }
 
 #endif // FILE_H
